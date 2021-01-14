b5f323b0d5418e16de78ed7805d9c48dc897e278
smallint <531476259@qq.com>
2021-01-14 10:18:57 +0800

    1、AI Codota插件

---
+++ src/javeSE/Codota/TestCodota.java
@@ -0,0 +1,13 @@
+package javeSE.Codota;
+
+/**
+ * @Classname TestCodota
+ * @Description TODO AI自动提示代码
+ * @Date 2021/1/14 10:14
+ * @Created by hmb.java.boy
+ */
+public class TestCodota {
+    public void testCodota(){
+        /** 首字母大写提示完成代码 */
+    }
+}
